68a7b6a299aa3111cec5d3e4c4cf6059a0f12725
azefgh456 <antonin.lesaout@sfr.fr>
2022-01-07 01:43:02 +0100

    Ajout d'une  methode d'injection de field avec cast pour contourner les nullpointer du au mauvais type de data

--- src/main/java/fr/acore/api/reflection/ReflectionHelper.java
+++ src/main/java/fr/acore/api/reflection/ReflectionHelper.java
@@ -51,4 +51,32 @@ public interface ReflectionHelper {
         return field;
     }
 
+    public default boolean injectField(Field field, Object instance, Object value) throws IllegalAccessException {
+        try{
+            if(!field.isAccessible()) field.setAccessible(true);
+
+            Class<?> fieldType = field.getType();
+            String serializedValue = String.valueOf(value);
+
+            if(fieldType.equals(int.class) || fieldType.equals(Integer.class)){
+                field.setInt(instance, Integer.parseInt(serializedValue));
+            }else if(fieldType.equals(double.class) || fieldType.equals(Double.class)){
+                field.setDouble(instance, Double.parseDouble(serializedValue));
+            }else if(fieldType.equals(float.class) || fieldType.equals(Float.class)){
+                field.setFloat(instance, Float.parseFloat(serializedValue));
+            }else if(fieldType.equals(boolean.class) || fieldType.equals(Boolean.class)){
+                field.setBoolean(instance, Boolean.parseBoolean(serializedValue));
+            }else if(fieldType.equals(long.class) || fieldType.equals(Long.class)){
+                field.setLong(instance, Long.parseLong(serializedValue));
+            }else{
+                field.set(instance, value);
+            }
+            return true;
+
+        }catch (Exception ex){
+            return false;
+        }
+
+    }
+
 }
